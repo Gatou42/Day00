78088d14d5dee24000d041969b17d05f502727d0
Gatou42 <gaetan.chazall@gmail.com>
2024-05-13 12:25:15 +0200

    Fait

---
+++ ex01/bonjourV2.py
@@ -0,0 +1,9 @@
+def main():
+    # Demander le prénom de l'utilisateur
+    prenom = input("Quel est votre prénom ? ")
+
+    # Répondre avec un message d'accueil incluant le prénom de l'utilisateur
+    print(f"Enchanté, moi c'est James, James Bond")
+
+if __name__ == "__main__":
+    main()
